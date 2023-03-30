52d094462a27901d11884d00926c215a9f0d4c51
Dilip lj <45902226+diliplj@users.noreply.github.com>
2023-03-30 08:47:02 +0000

    done
    done

--- second.py
+++ second.py
@@ -1,4 +1,4 @@
 print("hello jinkins !! finally you are configured with git hub")
 
-print("hello jinkins !! finally you are configured with git hub")
+print("hello jinkins !! finally you are configured withb")
 
